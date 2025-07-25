56a0e31f220012a89d7664a5c77a6e13fbebd11b
Idivya407 <bgayathri0012@gmail.com>
2025-07-25 12:43:31 +0530

    print args

--- src/main/java/com/vimandi/PlayWithGit/PlayWithGitApplication.java
+++ src/main/java/com/vimandi/PlayWithGit/PlayWithGitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PlayWithGitApplication {
 
 	public static void main(String[] args) {
+		System.out.println(args);
 		SpringApplication.run(PlayWithGitApplication.class, args);
 	}
 
