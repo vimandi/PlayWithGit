b5b8ddecad52635dfab8691f19ff461b057bd14e
Idivya407 <youremail@example.com>
2025-08-25 09:40:20 +0530

    partial changes using  git add -p .

--- src/main/java/com/vimandi/PlayWithGit/PlayWithGitApplication.java
+++ src/main/java/com/vimandi/PlayWithGit/PlayWithGitApplication.java
@@ -2,12 +2,14 @@ package com.vimandi.PlayWithGit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//add
 @SpringBootApplication
 public class PlayWithGitApplication {
 
 	public static void main(String[] args) {
+		//split1
 		System.out.println(args);
+		//split2
 		//adding commentss
 		SpringApplication.run(PlayWithGitApplication.class, args);
 	}
