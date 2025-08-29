23bdd2a5edb27ecee16e7d0d8452cd8fdc4fa374
Idivya407 <youremail@example.com>
2025-08-29 23:32:26 +0530

    Jaxb changes

---
+++ src/main/java/com/vimandi/Employee.java
@@ -0,0 +1,26 @@
+package com.vimandi;
+
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Employee {
+    private int id;
+    private String name;
+
+    @XmlElement
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @XmlElement
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/vimandi/JAXBMarshal.java
@@ -0,0 +1,33 @@
+package com.vimandi;
+
+import jakarta.xml.bind.*;
+
+import java.io.File;
+
+public class JAXBMarshal {
+    public static void main(String[] args) throws JAXBException {
+       /*
+        Employee emp = new Employee();
+        emp.setId(6126);
+        emp.setName("Divya imande");
+
+        JAXBContext context = JAXBContext.newInstance(Employee.class);
+
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+// Convert Java object to XML
+        marshaller.marshal(emp, new File("emp.xml"));
+
+*/
+        JAXBContext context = JAXBContext.newInstance(Employee.class);
+
+// Convert XML back to Java
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        Employee emp2 = (Employee) unmarshaller.unmarshal(new File("emp.xml"));
+System.out.println(emp2.getId() + " , "+emp2.getName());
+
+
+
+    }
+}
