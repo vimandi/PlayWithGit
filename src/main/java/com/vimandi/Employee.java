a7e9ff107e594539df5e7c8d6c0ac7fe0df69395
vimandi <131540744+vimandi@users.noreply.github.com>
2025-08-30 07:46:24 +0530

    Feature/f6 squash an merge commit
    * f6 a
    
    * f6 b

--- src/main/java/com/vimandi/Employee.java
+++ src/main/java/com/vimandi/Employee.java
@@ -3,6 +3,7 @@ package com.vimandi;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
+//f6 b
 @XmlRootElement
 public class Employee {
     private int id;
@@ -26,4 +27,6 @@ public class Employee {
 }
 
 
-//feature f7
+
+//f6 a
+
