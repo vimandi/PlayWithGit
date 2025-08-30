35d9158f64f75df6d7c8b37235aaafa1cfcd6f12
vimandi <vasu.imandhi@gmail.com>
2025-08-30 18:21:25 +0530

    alignment from m1

--- src/main/java/com/vimandi/JAXBMarshal.java
+++ src/main/java/com/vimandi/JAXBMarshal.java
@@ -6,7 +6,7 @@ import java.io.File;
 
 public class JAXBMarshal {
     public static void main(String[] args) throws JAXBException {
-       /*
+       
         Employee emp = new Employee();
         emp.setId(6126);
         emp.setName("Divya imande");
@@ -16,19 +16,19 @@ public class JAXBMarshal {
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
-// Convert Java object to XML
+		// Convert Java object to XML
         marshaller.marshal(emp, new File("emp.xml"));
 
-*/
-        /*
+
+        
         JAXBContext context = JAXBContext.newInstance(Employee.class);
 
-// Convert XML back to Java
+		// Convert XML back to Java
         Unmarshaller unmarshaller = context.createUnmarshaller();
         Employee emp2 = (Employee) unmarshaller.unmarshal(new File("emp.xml"));
         System.out.println("Employee Details : "+ emp2.getId() + " : "+emp2.getName());
 
-*/
+
 
     }
 }
