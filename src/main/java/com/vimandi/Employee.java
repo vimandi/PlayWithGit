cfff5d838cd501976c96911b4d3afc0439309a91
vimandi <vasu.imandhi@gmail.com>
2025-08-30 07:44:33 +0530

    f6 b

--- src/main/java/com/vimandi/Employee.java
+++ src/main/java/com/vimandi/Employee.java
@@ -3,6 +3,7 @@ package com.vimandi;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
+//f6 b
 @XmlRootElement
 public class Employee {
     private int id;
@@ -28,3 +29,4 @@ public class Employee {
 
 
 //f6 a
+
