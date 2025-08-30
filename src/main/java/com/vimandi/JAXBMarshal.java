06fe5fea17588f884380507686ce3d245fa922cf
vimandi <131540744+vimandi@users.noreply.github.com>
2025-08-30 18:00:26 +0530

    Implement marshaling (#29)

--- src/main/java/com/vimandi/JAXBMarshal.java
+++ src/main/java/com/vimandi/JAXBMarshal.java
@@ -26,7 +26,7 @@ public class JAXBMarshal {
 // Convert XML back to Java
         Unmarshaller unmarshaller = context.createUnmarshaller();
         Employee emp2 = (Employee) unmarshaller.unmarshal(new File("emp.xml"));
-System.out.println(emp2.getId() + " , "+emp2.getName());
+        System.out.println("Employee Details : "+ emp2.getId() + " : "+emp2.getName());
 
 */
 
