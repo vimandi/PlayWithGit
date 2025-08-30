eea7f2ec87378332e41bb26a0b91c1b5361e5bcc
vimandi <vasu.imandhi@gmail.com>
2025-08-30 18:52:47 +0530

    valid commit from L1

--- src/main/java/com/vimandi/JAXBMarshal.java
+++ src/main/java/com/vimandi/JAXBMarshal.java
@@ -15,17 +15,16 @@ public class JAXBMarshal {
 
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
 		// Convert Java object to XML
         marshaller.marshal(emp, new File("emp.xml"));
         
         JAXBContext context = JAXBContext.newInstance(Employee.class);
-
+        //some meaningful comment
 		// Convert XML back to Java
         Unmarshaller unmarshaller = context.createUnmarshaller();
         Employee emp2 = (Employee) unmarshaller.unmarshal(new File("emp.xml"));
-        System.out.println("Employee Details : "+ emp2.getId() + " : "+emp2.getName());
+        System.out.println("Employee Details are following : "+ emp2.getId() + " : "+emp2.getName());
 
 
 
